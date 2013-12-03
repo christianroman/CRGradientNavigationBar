c74c39d0fe6228e7280201a5f527ed4ed403018e
Robbie Bykowski <robbie.bykowski@gmail.com>
2013-12-03 09:31:56 +0000

    Make setBarTintGradientColors: more defensive. Remove an unneeded to constant.

--- CRGradientNavigationBar/CRGradientNavigationBar.m
+++ CRGradientNavigationBar/CRGradientNavigationBar.m
@@ -18,40 +18,65 @@
 @implementation CRGradientNavigationBar
 
 static CGFloat const kDefaultOpacity = 0.5f;
-static CGFloat const kstatusBarHeight = 20.0f;
 
 - (void)setBarTintGradientColors:(NSArray *)barTintGradientColors
 {
+    // create the gradient layer
     if (self.gradientLayer == nil) {
         self.gradientLayer = [CAGradientLayer layer];
         self.gradientLayer.opacity = self.translucent ? kDefaultOpacity : 1.0f;
-        [self.layer addSublayer:self.gradientLayer];
     }
     
-    NSMutableArray *barTintGradientCGColors = nil;
-    if (barTintGradientColors != nil) {
-        barTintGradientCGColors = [NSMutableArray arrayWithCapacity:[barTintGradientColors count]];
-        for (id color in barTintGradientColors) {
-            if ([color isKindOfClass:[UIColor class]]) {
-                [barTintGradientCGColors addObject:(id)[color CGColor]];
-            } else {
-                [barTintGradientCGColors addObject:color];
-            }
-        }
+    NSMutableArray * colors = nil;
+    if (barTintGradientColors != nil)
+    {
+        colors = [NSMutableArray arrayWithCapacity:[barTintGradientColors count]];
         
+        // determine elements in the array are colours
+        // and add them to the colors array
+        [barTintGradientColors enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            if ([obj isKindOfClass:[UIColor class]])
+            {
+                // UIColor class
+                [colors addObject:(id)[obj CGColor]];
+            }
+            else if ( CFGetTypeID( (__bridge void *)obj ) == CGColorGetTypeID() )
+            {
+                // CGColorRef
+                [colors addObject:obj];
+            }
+            else
+            {
+                // obj is not a supported type
+                @throw [NSException exceptionWithName:@"BarTintGraidentColorsError" reason:@"object in barTintGradientColors array is not a UIColor or CGColorRef" userInfo:nil];
+            }
+        }];
+        
+        // make it possible for the graident to be seen for iOS 6 and iOS 7
         if ( [self respondsToSelector:@selector(setBarTintColor:)] )
         {
+            // iOS 7
             self.barTintColor = [UIColor clearColor];
         }
         else
         {
+            // iOS 6
             self.tintColor = [UIColor clearColor];
             // stops the gradient on iOS 6 UINavigationBar
             [self setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
         }
     }
     
-    self.gradientLayer.colors = barTintGradientCGColors;
+    // set the graident colours to the laery
+    self.gradientLayer.colors = colors;
+}
+
+- (void)didMoveToWindow
+{
+    [super didMoveToWindow];
+    
+    // put the graident layer at index 1
+    [self.layer insertSublayer:self.gradientLayer atIndex:1];
 }
 
 #pragma mark - UIView
@@ -60,9 +85,20 @@ static CGFloat const kstatusBarHeight = 20.0f;
 {
     [super layoutSubviews];
     
-    if (self.gradientLayer != nil) {
-        self.gradientLayer.frame = CGRectMake(0, 0 - kstatusBarHeight, CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds) + kstatusBarHeight);
-        [self.layer insertSublayer:self.gradientLayer atIndex:1];
+    // allow all layout subviews call to adjust the frame
+    if ( self.gradientLayer != nil )
+    {
+        if ( floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_6_1 )
+        {
+             // iOS 7
+            CGFloat statusBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height;
+            self.gradientLayer.frame = CGRectMake(0, 0 - statusBarHeight, CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds) + statusBarHeight);
+        }
+        else
+        {
+            // iOS 6
+            self.gradientLayer.frame = CGRectMake(0, 0, CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds));
+        }
     }
 }
 

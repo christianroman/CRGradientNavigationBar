cdeff440ea19606d38c3bd7bedd8fe02533c4235
Sergey Popov <serji.popov@gmail.com>
2015-03-25 20:37:07 +0300

    Added test for testing some stuff.

--- GradientNavigationBarDemo/GradientNavigationBarDemoTests/GradientNavigationBarDemoTests.m
+++ GradientNavigationBarDemo/GradientNavigationBarDemoTests/GradientNavigationBarDemoTests.m
@@ -31,4 +31,60 @@
     XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
 
+- (void)testMath
+{
+    
+    for (int i = 0; i<360; i++)
+    {
+        [self t:i];
+    }
+    
+    
+}
+
+- (void)t:(int)deg
+{
+    double rad = deg * M_PI / 180;
+    
+    CGPoint A, B;
+    
+    
+    
+    if (rad < M_PI_4)
+    {
+        A = CGPointMake(0, (1 - tan(rad))/2);
+        B = CGPointMake(1, 1 - A.y);
+    }
+    else if (M_PI_4 <= rad && rad < M_PI_2)
+    {
+        B = CGPointMake(1/(2 *tan(rad)) + 1/2, 1);
+        A = CGPointMake(1 - B.x, 0);
+    }
+    else if (M_PI_2 <= rad)
+    {
+        B = CGPointMake(1/(2 *tan(rad)) + 1/2 + 1, 1);
+        A = CGPointMake(1 - B.x, 0);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    
+    
+    NSLog(@"%d", deg);
+    NSLog(@"%@ %@", NSStringFromCGPoint(A), NSStringFromCGPoint(B));
+    
+    
+}
+
+
+
 @end

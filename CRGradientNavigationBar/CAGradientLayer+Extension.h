987e0eeaadd5c39c60f73ce841842249685aa911
Sergey Popov <serji.popov@gmail.com>
2015-03-30 13:36:21 +0300

    Added category for setup gradient angle.

---
+++ CRGradientNavigationBar/CAGradientLayer+Extension.h
@@ -0,0 +1,15 @@
+//
+//  CAGradientLayer+Extension.h
+//  GradientNavigationBarDemo
+//
+//  Created by Sergey Popov on 30.03.15.
+//  Copyright (c) 2015 Sergey Popov. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface CAGradientLayer (Extension)
+
+@property (nonatomic, assign) CGFloat gradientAngle;
+
+@end
--- CRGradientNavigationBar/CRGradientNavigationBar.m
+++ CRGradientNavigationBar/CRGradientNavigationBar.m
@@ -8,9 +8,7 @@
 
 #import "CRGradientNavigationBar.h"
 #import <QuartzCore/QuartzCore.h>
-
-#define degreesToRadians(angleDegrees) (angleDegrees * M_PI / 180.0)
-#define SWAP(x, y) do { typeof(x) SWAP = x; x = y; y = SWAP; } while (0)
+#import "CAGradientLayer+Extension.h"
 
 @interface CRGradientNavigationBar ()
 
@@ -104,53 +102,7 @@
 
 - (void)setGradientAngle:(CGFloat)gradientAngle
 {
-    gradientAngle = fmod(gradientAngle, 360);
-    if (gradientAngle < 0)
-        gradientAngle += 360;
-    
-    double rad = degreesToRadians(gradientAngle);
-    double xa =0, ya=0, xb=0, yb=0;
-    
-    for (int n = 0; n < 4; n++)
-    {
-        if ((-M_PI_4+M_PI*n) <= rad && rad < (M_PI_4+M_PI*n))
-        {
-            xa = 0;
-            xb = 1;
-            ya = (1 - tan(rad))/2;
-            yb = 1 - ya;
-            
-            if (n % 2) {
-                SWAP(xa, xb);
-                SWAP(ya, yb);
-            }
-            
-            break;
-        }
-        else if ((M_PI_4+M_PI*n) <= rad && rad < (M_PI_4*3+M_PI*n))
-        {
-            yb = 1;
-            ya = 0;
-            
-            // xb > xa
-            xb = (1/tan(rad) + 1)/2;
-            
-            if (n > 0)
-            {
-                // xb<xa
-                xb = (1 - 1/tan(rad))/2;
-                SWAP(ya, yb);
-            }
-            
-            xa = 1 - xb;
-            
-            break;
-        }
-    }
-    
-    self.gradientLayer.startPoint = CGPointMake(xa, ya);
-    self.gradientLayer.endPoint = CGPointMake(xb, yb);
-
+    self.gradientLayer.gradientAngle = gradientAngle;
     _gradientAngle = gradientAngle;
 }
 
